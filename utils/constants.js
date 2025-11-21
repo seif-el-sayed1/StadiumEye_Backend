28775e3affd08cfc28c15779c3def86856a584c1
SeifElsayed <seifsayed121@gmail.com>
2025-11-21 14:31:24 +0200

    app constants

---
+++ utils/constants.js
@@ -0,0 +1,12 @@
+exports.ADMIN = "admin";
+exports.SUPERVISOR = "supervisor";
+exports.FAN = "fan";
+exports.VISITOR = "visitor";
+
+exports.ROLES = [exports.ADMIN, exports.SUPERVISOR, exports.FAN, exports.VISITOR];
+
+exports.LOGIN_TYPE_LIST = ["apple", "google", "email", "social"];
+exports.LOGIN_TYPE_PLATFORM_LIST = ["apple", "google", "social"];
+exports.GENDER_LIST_EN = ["male", "female"];
+exports.GENDER_LIST_AR = ["ذكر", "أنثى", "أنثي", "انثي", "انثى"];
+exports.SERVICES_LIST = ["parking", "seats", "entry", "security"]
