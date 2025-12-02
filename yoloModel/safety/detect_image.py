ec8db670b86ef21eb6f237264486306bdfc4cf5e
SeifElsayed <seifsayed121@gmail.com>
2025-12-02 21:39:53 +0200

    python safety script for detect image

---
+++ yoloModel/safety/detect_image.py
@@ -0,0 +1,47 @@
+﻿import sys
+import json
+from ultralytics import YOLO
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "yolo_model.pt")
+
+# Load YOLO model
+model = YOLO(model_path)
+
+input_path = sys.argv[1]
+
+results = model(input_path, verbose=False)
+
+allowed_classes = [
+    "NO-Gloves",
+    "NO-Goggles",
+    "NO-Hardhat",
+    "NO-Mask",
+    "NO-Safety Vest",
+    "Fall-Detected"
+]
+
+detection_data = []
+boxes = results[0].boxes
+if boxes is not None:
+    for box in boxes:
+        cls = int(box.cls[0])
+        conf = float(box.conf[0])
+        x1, y1, x2, y2 = box.xyxy[0]
+        width = float(x2 - x1)
+        height = float(y2 - y1)
+        class_name = allowed_classes[cls]
+
+        if class_name in allowed_classes:
+            detection_data.append({
+                "Id": cls,
+                "ClassName": class_name,
+                "confidence": round(conf, 2),
+                "x": round(float(x1)),
+                "y": round(float(y1)),
+                "width": round(width),
+                "height": round(height)
+            })
+
+print(json.dumps(detection_data))
