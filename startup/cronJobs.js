6d14317e4b83bd821edfac9551dd6f268b5bc57e
seif_elsayed <seifsayed121@gmail.com>
2026-07-09 20:50:00 +0300

    update cron job to sync venues by country instead of updating stadium ratings

--- startup/cronJobs.js
+++ startup/cronJobs.js
@@ -1,28 +1,37 @@
 const cron = require("node-cron");
 const Stadium = require("../models/stadium.model");
 const getGoogleRating = require("../utils/googleRating");
+const { syncVenuesByCountry } = require("../utils/footballApi");
 
-const updateAllStadiumRatings = () => {
-    cron.schedule("0 0 */14 * *", async () => { 
+module.exports = () => {
+    // cron.schedule("0 0 */14 * *", async () => {
+    //     try {
+    //         const stadiums = await Stadium.find();
+
+    //         for (const stadium of stadiums) {
+    //             const ratingData = await getGoogleRating(stadium.stadiumName);
+    //             await Stadium.updateOne(
+    //                 { _id: stadium._id },
+    //                 {
+    //                     rate: ratingData.rating,
+    //                     reviewsQuantity: ratingData.reviewsCount
+    //                 }
+    //             );
+    //         }
+
+    //         console.log(`All stadium ratings updated successfully`.green.bold);
+    //     } catch (error) {
+    //         console.error("Error updating stadium ratings:", error.message.red);
+    //     }
+    // });
+
+    cron.schedule("0 0 */14 * *", async () => {
         try {
-            const stadiums = await Stadium.find();
-
-            for (const stadium of stadiums) {
-                const ratingData = await getGoogleRating(stadium.stadiumName);
-                await Stadium.updateOne(
-                    { _id: stadium._id },
-                    {
-                        rate: ratingData.rating,
-                        reviewsQuantity: ratingData.reviewsCount
-                    }
-                );
-            }
-
-            console.log(`All stadium ratings updated successfully`.green.bold);
+            await syncVenuesByCountry("Saudi-Arabia");
         } catch (error) {
-            console.error("Error updating stadium ratings:", error.message.red);
+            console.error("Error syncing Saudi venues:", error.message.red);
         }
     });
-};
 
-module.exports = updateAllStadiumRatings;
+    console.log("Cron jobs initialized".cyan.bold);
+};
